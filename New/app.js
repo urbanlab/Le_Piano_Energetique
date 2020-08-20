111939da92c97214914ea48695a6e14bf66facc9
Jeremie <JeremieForge@users.noreply.github.com>
2020-08-20 18:30:49 +0200

    player

--- New/app.js
+++ New/app.js
@@ -14,16 +14,19 @@ let port = 3000;
 
 
 var arduinoData = [
-  { name: 'btn1', val:0  },
-  { name: 'btn2', val:0  },
-  { name: 'btn3', val:0  },
-  { name: 'sensor1', val:0  }
+  { name: 'inter1', val:0  },
+  { name: 'inter2', val:0  },
+  { name: 'inter3', val:0  },
+  { name: 'inter4', val:0  },
+  { name: 'inter5', val:0  },
+  { name: 'sensor1', val:0  },
+  { name: 'sensor2', val:0  }
 ];
 
 
 // Set the app instance to read the public directory
 // Will find index.html
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/player'));
 
 // board.on
 board.on("ready", function() {
@@ -41,15 +44,21 @@ board.on("ready", function() {
   //   io.emit('data', this.value);
   // });
 
-  var btn1 = new five.Pin(3);
-  btn1.read(function(error, value) { arduinoData[0].val = value; });
-  var btn2 = new five.Pin(4);
-  btn2.read(function(error, value) { arduinoData[1].val = value; });
-  var btn3 = new five.Pin(5);
-  btn3.read(function(error, value) { arduinoData[2].val = value; });
+  var inter1 = new five.Pin(3);
+  inter1.read(function(error, value) { arduinoData[0].val = value; });
+  var inter2 = new five.Pin(4);
+  inter2.read(function(error, value) { arduinoData[1].val = value; });
+  var inter3 = new five.Pin(5);
+  inter3.read(function(error, value) { arduinoData[2].val = value; });
+  var inter4 = new five.Pin(6);
+  inter4.read(function(error, value) { arduinoData[3].val = value; });
+  var inter5 = new five.Pin(7);
+  inter5.read(function(error, value) { arduinoData[4].val = value; });
 
-  var sensor = new five.Sensor("A0");
-  sensor.on("change", function() { arduinoData[3].val = this.value; });
+  var pot1 = new five.Sensor("A0");
+  pot1.on("change", function() { arduinoData[5].val = this.value; });
+  var pot2 = new five.Sensor("A1");
+  pot2.on("change", function() { arduinoData[6].val = this.value; });
 
   setInterval(() => {
     console.log(arduinoData);
