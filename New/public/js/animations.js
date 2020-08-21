97e3e816bb5d2bc13118d700e8bdf9416143d10a
Antoine Josnin <antoine@akago.fr>
2020-08-21 12:09:15 +0200

    overlay animations

--- New/public/js/animations.js
+++ New/public/js/animations.js
@@ -1,7 +1,21 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById("lottieContainer"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "assets/unicorn.json",
-});
+function runAnimation(fileName) {
+  bodymovin.loadAnimation({
+    container: document.getElementById(fileName + "Container"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "assets/" + fileName + ".json",
+  });
+}
+
+function fadeIn(id) {
+  var el = document.getElementById(id);
+  el.classList.add("show");
+  el.classList.remove("hide");
+}
+
+function fadeOut(id) {
+  var el = document.getElementById(id);
+  el.classList.add("hide");
+  el.classList.remove("show");
+}
--- New/public/js/index.js
+++ New/public/js/index.js
@@ -1,6 +1,22 @@
 let socket = io("192.168.81.27:3000");
 
 // When data is 'recieved' do something
-socket.on("data", function (val) {
+// socket.on("data", function (val) {
+//   console.log(val);
+// });
+
+socket.on("inter1", function (val) {
   console.log(val);
 });
+
+runAnimation("unicorn");
+runAnimation("rocket");
+runAnimation("tick");
+runAnimation("world");
+// runAnimation("hills");
+
+fadeIn("rocketContainer");
+fadeOut("tickContainer");
+fadeOut("worldContainer");
+fadeIn("unicornContainer");
+// fadeOut("hillsContainer");
