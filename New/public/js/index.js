6761a70e66edcf0357f83a7be4b8bcd577285991
Antoine Josnin <antoine@akago.fr>
2020-08-21 14:46:26 +0200

    first try with custom animation

--- New/public/js/index.js
+++ New/public/js/index.js
@@ -1,22 +1,35 @@
 let socket = io("192.168.81.27:3000");
 
+let defaultState = {
+  "inter1": 0,
+  "inter2": 0,
+  "inter3": 0,
+  "inter4": 0,
+  "inter5": 0,
+  "pot1": 0,
+  "pot2": 0,
+
+}
 // When data is 'recieved' do something
 // socket.on("data", function (val) {
 //   console.log(val);
 // });
 
 socket.on("inter1", function (val) {
-  console.log(val);
+  console.log("inter1 : " + val);
+});
+socket.on("inter2", function (val) {
+  console.log("inter2 : " + val);
 });
 
-runAnimation("unicorn");
-runAnimation("rocket");
-runAnimation("tick");
-runAnimation("world");
-// runAnimation("hills");
+runAnimation("plantbubbles");
+// runAnimation("rocket");
+// runAnimation("tick");
+// runAnimation("world");
+// // runAnimation("hills");
 
-fadeIn("rocketContainer");
-fadeOut("tickContainer");
-fadeOut("worldContainer");
-fadeIn("unicornContainer");
+// fadeIn("rocketContainer");
+// fadeOut("tickContainer");
+// fadeOut("worldContainer");
+// fadeIn("unicornContainer");
 // fadeOut("hillsContainer");
